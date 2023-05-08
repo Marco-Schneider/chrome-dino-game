190cedd1a991f47ae8de799fd9ff386509b0b9c9
marco-schneider <marco.schneider452@gmail.com>
2023-05-08 17:31:39 -0300

    Added jumping to the character

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 console.log("Loaded the javaScript file!");
+
+var character = document.getElementById("game-character");
+
+document.onkeydown = (e) => {
+  e = e || window.event;
+  if(e.keyCode == 38) {
+    character.classList.add("chromium-dino-jump");
+  }
+}
+
+character.addEventListener("animationend", () => {
+  character.classList.remove("chromium-dino-jump");
+})
