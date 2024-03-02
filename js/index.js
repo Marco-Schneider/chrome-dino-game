d7f77efb47b1fa97c0615182e83f3dad7eda5f10
Marco-Schneider <marco.schneider452@gmail.com>
2024-03-02 00:47:13 -0300

    Jumping functionality and minor tweaks

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ console.log("Loaded the javaScript file!");
 
 var character = document.getElementById("game-character");
 
-document.onkeydown = (e) => {
+document.onkeyup = (e) => {
   e = e || window.event;
   if(e.keyCode == 38) {
     character.classList.add("chromium-dino-jump");
